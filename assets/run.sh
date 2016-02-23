8670bef125cbe6c0003115973dd06b05a3ead696
aresetian <carlosandresgg@gmail.com>
2016-02-23 17:02:10 -0500

    changes the name of the log file

--- assets/run.sh
+++ assets/run.sh
@@ -3,15 +3,15 @@
 
 #Config Logs
 export LOG_PATH=${LOG_PATH:-/logs/}
-export LOG_NAME=${LOG_NAME:-Hotetec_$HOSTNAME.log}
+export LOG_NAME=${LOG_NAME:-ServiceTest_$HOSTNAME.log}
 export LOG_LEVEL=${LOG_LEVEL:-1} 
 #LevelDebug = 1,        LevelInfo  = 2, LevelWarn  = 3, LevelError = 4
 
 
 
 echo "Generating config.ini..."
-envsubst < /.setup/config.ini > /go/src/golandservicetest/golangservicetest/config.ini
+envsubst < /.setup/config.ini > /go/src/golangservicetest/golangservicetest/config.ini
 
 
 # Start dataservice
-cd /go/src/golandservicetest/golangservicetest/ && ./golangservicetest -fileconfig=config.ini
+cd /go/src/golangservicetest/golangservicetest/ && ./golangservicetest -fileconfig=config.ini
